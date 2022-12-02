5db3b995cc030c13449ab43988f7485004cde13b
Your Name <you@example.com>
2022-12-02 22:41:51 +0800

    Js done

--- login.js
+++ login.js
@@ -11,3 +11,45 @@ const block = document.querySelector(".block"),
         block.classList.remove("active");
     });
 
+
+    var users
+
+    function initUsers() {
+      users = [
+        {
+            "email":"hanielbyu@gmail.com",
+            "password":"0000"
+        },
+        {
+            "email":"s10247873@connect.np.edu.sg",
+            "password":"0001"
+        }
+      ]
+      return users
+    }
+// const found = array1.find(o => o.email === 'hanielbyu@gmail.com');
+
+function login() {
+    const email = document.getElementById("email").value
+    const password = document.getElementById("password").value
+    let users = initUsers()
+    const found = users.find(o => o.email === email)
+    if(found){
+        if (password === found.password){
+            console.log(" Login succesful")
+            window.location.href = "index.html"
+        }
+        else{
+            console.log("login failed")
+            alert("Invalid email or password")
+        }
+    }
+    else {
+        console.log("login failed")
+        alert("Invalid email or password")
+    }
+}
+
+// function signup(){
+//     const email = document.getElementById("email").value
+// }
